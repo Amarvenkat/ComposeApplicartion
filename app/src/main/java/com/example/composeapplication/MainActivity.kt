587bb402af41360527e86807336825ba5f454c80
Amarvenkat <amarvenkat30@gmail.com>
2023-02-09 12:10:00 +0530

    First Compose

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -11,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
@@ -30,7 +33,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-   Column(horizontalAlignment =  Alignment.CenterHorizontally) {
+   Column(
+       modifier = Modifier
+           //.fillMaxSize() // match parent and match parent
+           .fillMaxSize(0.5f)// 1 is max i.e 100% 0.5f is 500%
+           .background(Color.Gray),
+       horizontalAlignment =  Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center
+   ) {
        Text(text = "Hello")
        Text(text = "Amar")
    }
